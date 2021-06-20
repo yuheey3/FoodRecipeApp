8c438b88b448244232367c5198cb8baf8c5113fb
Yuki Waka <yuheey3@yahoo.co.jp>
2021-06-19 23:13:04 -0400

    * RandomFood.cs:
    * FoodRecipeApp.csproj: Random food Class

---
+++ FoodRecipeApp/Model/RandomFood.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipeApp.Model
+{
+    public class RandomFood
+    {
+        public string idMeal { get; set; }
+        public string strMeal { get; set; }
+        public string strCategory { get; set; }
+        public string strArea { get; set; }
+        public string strInstructions { get; set; }
+        public string strMealThumb { get; set; }
+        public string strTags { get; set; }
+        public string strYoutube { get; set; }
+    }
+
+}
